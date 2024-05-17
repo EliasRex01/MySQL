feecef2d02b861153c4a01b15d55384cb61dbe91
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-17 14:13:29 -0400

    Update limitar.sql

--- limitar.sql
+++ limitar.sql
@@ -12,7 +12,6 @@ SELECT Name, Population FROM city ORDER BY Population DESC LIMIT 5;
 SELECT Name FROM city ORDER BY RAND() LIMIT 5;
 
 
-
 -- Limitar resultados por paginas
 SELECT * FROM city
 LIMIT 5,5;
