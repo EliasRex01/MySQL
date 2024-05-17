c598d50387be8b07496d3ff100d863470d1b1f38
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-17 14:03:28 -0400

    Update Crear BD.sql

--- Crear BD.sql
+++ Crear BD.sql
@@ -1,9 +1,6 @@
 -- Crear base de datos nueva
 #CREATE DATABASE nueva_BD;
 
--- Ver todas las bases de datos del sistema
--- SHOW DATABASES;
-
 -- Crear usuario nuevo con password
 #CREATE USER 'nuevo_usuario'@'localhost' IDENTIFIED BY '1234';
 
@@ -13,16 +10,12 @@
 
 -- para usar una base de datos
 #USE bd1;
--- ver las tablas de esa base de datos
-#SHIW TABLES;
--- describe la tabla con algunos detalles
-#DESCRIBE nomdetabla;
+
 
 #En SQL primero se ingresa el comando y luego la sentencia
 
 -- para instalar una tabla descargada
 -- primeramente en cmd ingreso mysql -uroot -p y luego la contra
--- SHOW databases; para ver las bd que tengo
 -- para crear bd en cmd se usa   create database world; por ejemplo
 -- para enlazar a un archivo sql se pone use world; en este caso
 -- luego abajo se pone SOURCE Downloads\\world.sql; (o carpeta donde este)
