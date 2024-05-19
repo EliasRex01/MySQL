f01e5e125fff6472324a5d63eb486ada2aee1a3d
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-19 15:16:24 -0400

    Create BD Ahorro Ejercicio.sql

---
+++ Actividades SQL/BD Ahorro Ejercicio.sql
@@ -0,0 +1,9 @@
+-- El HC Bank nos ha facilitado la base que contiene su cartera de clientes de cuentas de ahorro y
+-- nos ha solicitado obtener datos para el análisis de las operaciones sospechosas. Los datos
+-- solicitados deben ser idénticos a como se indican a continuación:
+-- 1. Obtenga todas las cuentas de ahorro de los 2 clientes (bajo sospecha de lavado de
+-- dinero) indicados abajo. El resultado debe ser idéntico al cuadro:
+
+
+
+
