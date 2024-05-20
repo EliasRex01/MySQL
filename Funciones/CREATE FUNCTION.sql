1675decb68fcd71d73f3d80d8ca740ee4fd08edf
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-20 00:15:50 -0400

    Update CREATE FUNCTION.sql

--- Funciones/CREATE FUNCTION.sql
+++ Funciones/CREATE FUNCTION.sql
@@ -3,62 +3,47 @@
 -- es parecido al PA pero va aplicado a realizar una determinada tarea
 
 -- Sintaxis es
--- CREATE FUNCTION 'nueva funcion' ()
--- RETURNS INTEGER
+-- CREATE OR REPLACE FUNCTION nueva_funcion()
+-- RETURNS INTEGER AS $$
 -- BEGIN
-
--- RETURN 1;
--- END
-
-
--- Ejemplo de una funcion
-delimiter $$
-	CREATE FUNCTION `HolaMundo` ()
-	RETURNS varchar(30)
-	DETERMINISTIC
-	BEGIN
-		RETURN 'Hola Mundo de MySQL Server';
-	END;
-$$
-
--- llamar a la funcion con  select
-select HolaMundo();
+--     RETURN 1;
+-- END;
+-- $$ LANGUAGE plpgsql;
 
 
 -- Ejemplo de funcion suma
-delimiter $$
-	create function Suma(Valor1 int, Valor2 int) returns int
-    deterministic
-    begin
-		declare Resultado int;
-        set Resultado = Valor1+Valor2;
-		return Resultado;
-	end;
-$$
+CREATE OR REPLACE FUNCTION sumar(Valor1 INTEGER, Valor2 INTEGER)
+RETURNS INTEGER AS $$
+BEGIN
+    RETURN Valor1 + Valor2;
+END;
+$$ LANGUAGE plpgsql;
 
 select Suma(3,5);
 
 
 -- Ejemplo de funcion mas compleja
-delimiter $$
-	create function Operacion_full(Valor1 int,
-									Valor2 int,
-                                    Op varchar(15)) 
-	returns int
-    deterministic
-    begin
-		declare Resultado int;
-        if Op = 'SUMA' then
-			set Resultado = Valor1+Valor2;
-        elseif Op = 'RESTA' then
-			set Resultado = Valor1-Valor2;
-		elseif Op = 'MULTIPLICA' then
-            set Resultado = Valor1*Valor2;
-		else
-			set Resultado=0;
-		end if;
-		return Resultado;
-	end;
-$$
+
+CREATE OR REPLACE FUNCTION operacion_full(
+    valor1 INT,
+    valor2 INT,
+    op VARCHAR(15)
+) 
+RETURNS INT AS $$
+DECLARE
+    resultado INT;
+BEGIN
+    IF op = 'SUMA' THEN
+        resultado = valor1 + valor2;
+    ELSIF op = 'RESTA' THEN
+        resultado = valor1 - valor2;
+    ELSIF op = 'MULTIPLICA' THEN
+        resultado = valor1 * valor2;
+    ELSE
+        resultado = 0;
+    END IF;
+    RETURN resultado;
+END;
+$$ LANGUAGE plpgsql;
 
 select Operacion_full(3,5,'RESTA');
