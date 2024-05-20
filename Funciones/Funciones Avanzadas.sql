848001bdd942edcb86ccc5f59b86586875750d81
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-20 12:15:14 -0400

    Update Funciones Avanzadas.sql

--- Funciones/Funciones Avanzadas.sql
+++ Funciones/Funciones Avanzadas.sql
@@ -3,26 +3,26 @@
 -- Funcion CAST()
 -- hace una conversion de tipo de dato para el resultado
 
-select '2022-08-09';              -- es una cadena
+SELECT '2022-08-09';              -- es una cadena
 select cast('2022-08-09' as date);    -- convirtiendo a tipo fecha
 
-
-select concat(cast(124 as char), 'ruta');
+-- Concatenar una cadena con un número convertido a cadena
+SELECT CAST(124 AS TEXT) || 'ruta';
 
 -- is null evalua si la expresion es nula o no 
 -- 1 es nulo pero 0 es no nulo
 
--- funcion if()
+
 --        si   , pasa esto,      si no esto
-select if(x=5, 'datos iguales', 'datos diferentes');
+SELECT CASE WHEN x = 5 THEN 'datos iguales' ELSE 'datos diferentes' END;
 
 -- convert permite hacer conversiones de dato, parecido a cast pero con diferente sintaxis
-select convert('2020-08-09', date);      
+SELECT CAST('2022-08-09' AS DATE);     
 
--- case evalua casos dependiendo de la alternativa brindada
-select (case 1                          -- se evalua en 1 aca, se puede reemplazar y evalua en ese valor
-		when 1 then 'uno'
-        when 2 then 'tres'
-        when 3 then 'cuatro'
-        else 'no encontrado' 
-        end);
+-- case evalua casos dependiendo de la alternativa brindada		
+SELECT CASE 1                     -- se evalua en 1 aca, se puede reemplazar y evalua en ese valor
+    WHEN 1 THEN 'uno'
+    WHEN 2 THEN 'tres'
+    WHEN 3 THEN 'cuatro'
+    ELSE 'no encontrado'
+END;
