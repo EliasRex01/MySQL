995439830b97e6e8d47f51156b562cf41b12f455
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-22 11:04:22 -0400

    Update CA 2 (select, join).sql

--- Actividades SQL/CA 2 (select, join).sql
+++ Actividades SQL/CA 2 (select, join).sql
@@ -36,3 +36,21 @@ SELECT * FROM tabla1;
 
 -- crear normal es solo CREATE TABLE tabla1 AS (no temporal)
 
+-- creacion de la vista
+CREATE VIEW vista1 AS
+(SELECT COUNT(*), a.codigo_articulo, ta.descripciom_tipo,
+a.codigo_sucursal     -- se podria quitar el codigo_articulo para mostrar por grupos
+FROM articulos a JOIN tipo_articulos ta
+ON a.codigo_tipo = ta.codigo_tipo
+JOIN articulos_sucursal asu
+ON a.codigo_articulo = asu.codigo_articulo
+JOIN sucursales s
+ON asu.codigo_sucursal = s.codigo_sucursal
+JOIN ciudades c
+ON s.codigo_sucursal = c.codigo_sucursal
+GROUP BY a.codigo_articulo, ta.descripcion_tipo, 
+  s.codigo_sucursal, c.nombre_ciudad
+ORDER BY c.nombre_ciudad);
+
+-- consultar la vista
+SELECT * FROM vista1;
