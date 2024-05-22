b5000cd15156cc7c28692983a744a68cb196adfc
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-21 21:29:24 -0400

    Update martes 21 de mayo.sql

--- Actividades SQL/martes 21 de mayo.sql
+++ Actividades SQL/martes 21 de mayo.sql
@@ -22,3 +22,13 @@ between '01/01/2016'and '31/12/2016'
 group by p.anho
 order by 1 desc
 limit 5;
+
+-- el top 10 de los clientes que mas me compraron
+select c.nombre, sum(p.total) as "valor compra" 
+from pedidos p
+join clientes c
+on c.ruc = p.ruc
+group by c.nombre
+order by sum(total) desc
+limit 10;
+
