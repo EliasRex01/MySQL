c8c4b62eeda041671816a2aaf74cb09fa7e4b5a6
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-22 11:01:39 -0400

    Update CA 2 (select, join).sql

--- Actividades SQL/CA 2 (select, join).sql
+++ Actividades SQL/CA 2 (select, join).sql
@@ -14,3 +14,25 @@ GROUP BY a.codigo_articulo, ta.descripcion_tipo,
   s.codigo_sucursal, c.nombre_ciudad
 ORDER BY c.nombre_ciudad;
 
+-- crear tabla en base a consulta (tabla auxiliar o tabla temporal)
+-- solo existe durante la sesion abierta (en el bufer)
+CREATE TEMP TABLE tabla1 AS
+(SELECT COUNT(*), a.codigo_articulo, ta.descripciom_tipo,
+a.codigo_sucursal     -- se podria quitar el codigo_articulo para mostrar por grupos
+FROM articulos a JOIN tipo_articulos ta
+ON a.codigo_tipo = ta.codigo_tipo
+JOIN articulos_sucursal asu
+ON a.codigo_articulo = asu.codigo_articulo
+JOIN sucursales s
+ON asu.codigo_sucursal = s.codigo_sucursal
+JOIN ciudades c
+ON s.codigo_sucursal = c.codigo_sucursal
+GROUP BY a.codigo_articulo, ta.descripcion_tipo, 
+  s.codigo_sucursal, c.nombre_ciudad
+ORDER BY c.nombre_ciudad);
+
+-- consultar tabla auxiliar
+SELECT * FROM tabla1;
+
+-- crear normal es solo CREATE TABLE tabla1 AS (no temporal)
+
