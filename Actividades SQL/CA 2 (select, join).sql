02b03b659074b4468abf10a95122ca0f0344f596
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-22 10:54:26 -0400

    Update CA 2 (select, join).sql

--- Actividades SQL/CA 2 (select, join).sql
+++ Actividades SQL/CA 2 (select, join).sql
@@ -1,7 +1,7 @@
 -- contar los articulos por cada tipo, por sucursal, mostrar el codigo del articulo
 -- la descripcion del tipo de articulo, el codigo sucursal y el nombre de la ciudad
 SELECT COUNT(*), a.codigo_articulo, ta.descripciom_tipo,
-a.codigo_sucursal
+a.codigo_sucursal     -- se podria quitar el codigo_articulo para mostrar por grupos
 FROM articulos a JOIN tipo_articulos ta
 ON a.codigo_tipo = ta.codigo_tipo
 JOIN articulos_sucursal asu
@@ -13,3 +13,4 @@ ON s.codigo_sucursal = c.codigo_sucursal
 GROUP BY a.codigo_articulo, ta.descripcion_tipo, 
   s.codigo_sucursal, c.nombre_ciudad
 ORDER BY c.nombre_ciudad;
+
