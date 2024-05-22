bd228d0d7dbc98ff9bbc85bee58e241705415f8c
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-21 21:09:51 -0400

    Update martes 21 de mayo.sql

--- Actividades SQL/martes 21 de mayo.sql
+++ Actividades SQL/martes 21 de mayo.sql
@@ -5,3 +5,11 @@ and '31/01/2016'
 limit 5;
 
 
+-- que dia hubo mas pedidos
+select fecha "Fecha", count(*) "Cantidad", sum(total) "Monto Total" 
+from pedidos p
+where fecha 
+between '01/01/2016'and '31/01/2016'
+group by fecha
+order by 2 desc
+limit 5;
