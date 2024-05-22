17867980be26a3a24ab3403308b4a230c965267b
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-22 13:18:29 -0400

    Create CA 3 Funciones.sql

---
+++ Actividades SQL/CA 3 Funciones.sql
@@ -0,0 +1,4 @@
+-- caso 3 de la actividad 4.4 donde se implementa codigo - ARTICULO codigo
+
+SELECT p.desc_proveedor AS "PROVEEDORES", COUNT(*) AS "ARTICULOS",
+MIN(ultimo_costo) AS "COSTO MAS BARATO",
