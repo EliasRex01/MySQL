622b18ea56966a786cfabb97dbc6b5366e4a1e9e
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-21 21:22:10 -0400

    Update martes 21 de mayo.sql

--- Actividades SQL/martes 21 de mayo.sql
+++ Actividades SQL/martes 21 de mayo.sql
@@ -14,4 +14,11 @@ group by fecha
 order by 2 desc
 limit 5;
 
-
+-- El total de las ventas del año 2016, incluya el año
+select p.anho as "Año", sum(total) as "Monto total" 
+from pedidos p
+where fecha 
+between '01/01/2016'and '31/12/2016'
+group by p.anho
+order by 1 desc
+limit 5;
