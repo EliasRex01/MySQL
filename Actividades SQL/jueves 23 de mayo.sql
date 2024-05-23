20bc84505816f584248ea031ac29332c80f3e753
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-23 19:52:35 -0400

    Update jueves 23 de mayo.sql

--- Actividades SQL/jueves 23 de mayo.sql
+++ Actividades SQL/jueves 23 de mayo.sql
@@ -7,7 +7,11 @@ select * from cuentas where codigo_cliente in (select cl.codigo_cliente
 												   where cl.nombres 
 												   ilike '%Gamarra%');
 
-
+-- ahorristas cuyo apellido es gamarra (50 son personas, osea un cliente puede tener mas de una cuenta)
+select count(distinct c.codigo_cliente) from cuentas c where codigo_cliente in (select cl.codigo_cliente
+												   from clientes cl 
+												   where cl.nombres 
+												   ilike '%Gamarra%');
 
 
 
