d79dce65f608a3762c1b112ad002860a42ddd7c0
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-23 20:02:12 -0400

    Update jueves 23 de mayo.sql

--- Actividades SQL/jueves 23 de mayo.sql
+++ Actividades SQL/jueves 23 de mayo.sql
@@ -13,5 +13,10 @@ select count(distinct c.codigo_cliente) from cuentas c where codigo_cliente in (
 												   where cl.nombres 
 												   ilike '%Gamarra%');
 
-
+-- usando join, por cada cliente la cantidad de cuentas que tiene
+select cl.nombres, count(cu.codigo_cuenta) as cantidad from clientes cl
+join cuentas cu on cl.codigo_cliente = cu.codigo_cliente
+group by cl.nombres
+order by 2 desc
+limit 10;
 
