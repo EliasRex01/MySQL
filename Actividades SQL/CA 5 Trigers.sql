b18547ece01b1d9bce41114e4b815706b153ad47
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-25 14:08:50 -0400

    Update CA 5 Trigers.sql

--- Actividades SQL/CA 5 Trigers.sql
+++ Actividades SQL/CA 5 Trigers.sql
@@ -1,1 +1,10 @@
--- el triger se hace sobre la tabla que queremos disparar
+-- el triger se hace sobre la tabla que queremos disparar el triger
+
+-- creacion de un trigger para insertar
+CREATE TRIGGER ins_stock               -- nombre del trigger
+    ON pedidos_articulos               -- se indica sobre que tabla debe actuar
+    AFTER INSERT                       -- que accion disparara este trigger
+AS
+BEGIN
+
+    SET NOCOUNT ON      -- previene que existan resultados extras que interfieran con el trigger
