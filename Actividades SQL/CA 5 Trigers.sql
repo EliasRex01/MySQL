4c061c48d9a9b5babd8ae7ca6b53579a2591fb11
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-25 15:29:27 -0400

    Update CA 5 Trigers.sql

--- Actividades SQL/CA 5 Trigers.sql
+++ Actividades SQL/CA 5 Trigers.sql
@@ -61,3 +61,49 @@ values (85700, 10010058866, 105262, 100000, 10, 2016, 49781)    -- venta de 10
 
 -- ver los cambios 
 select * from articulos_sucursal where codigo_articulo = 105262;  
+
+
+
+
+
+-- creacion de un trigger para borrar
+CREATE TRIGGER del_stock               -- nombre del trigger
+    ON pedidos_articulos               -- se indica sobre que tabla debe actuar
+    AFTER DELETE                       -- que accion disparara este trigger
+AS 
+DECLARE
+    @ca int,
+    @anio int,
+    @num int,
+    @cs int,
+    @cant int,
+    @cantart int
+BEGIN
+
+    SET NOCOUNT ON      -- previene que existan resultados extras que interfieran con el trigger
+
+    -- actualizar el stock de una sucursal, se necesita cod_sucursal y cod_articulo
+    select @ca = codigo_articulo from inserted; -- a la par que se recupera se guarda en ca
+    select @anio = anho from inserted;
+    select @num = numero_pedido from inserted;
+    select @cant = cantidad from inserted;
+
+    -- inserted es la tabla temporal del insert  (en este caso es copia de predidos_articulos)
+
+    select @cs = codigo_sucursal from pedidos     -- codigo_sucursal se recupera de pedidos
+    where anho = @anio and numero_pedido = @num
+
+    -- obtener la cantidad que tenemos en articulo_sucursal (se recupera)
+    select @cantart = stock from articulos_sucursal
+    where codigo_articulo = @ca and codigo_sucursal = @cs
+
+    if (@cant < @cantart)     -- si cantidad a vender es menor a cantidad que tengo
+    begin
+        -- dato a actualizar del stock. (update sobre articulos_sucursal para el campo stock)
+        update articulos_sucursal
+        set stock = stock - @cant            -- una venta, osea aumenta el pedido y baja el stock
+        where codigo_articulo = @ca and codigo_sucursal = @cs
+    end
+        
+END
+GO
