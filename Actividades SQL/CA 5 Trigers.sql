c06096390d0fb9eccb3e7cd63474fee2cfb97c0d
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-25 14:40:15 -0400

    Update CA 5 Trigers.sql

--- Actividades SQL/CA 5 Trigers.sql
+++ Actividades SQL/CA 5 Trigers.sql
@@ -4,7 +4,18 @@
 CREATE TRIGGER ins_stock               -- nombre del trigger
     ON pedidos_articulos               -- se indica sobre que tabla debe actuar
     AFTER INSERT                       -- que accion disparara este trigger
-AS
+AS 
+DECLARE
+    @codigo_articulo int
 BEGIN
 
     SET NOCOUNT ON      -- previene que existan resultados extras que interfieran con el trigger
+
+    -- actualizar el stock de una sucursal, se necesita cod_sucursal y cod_articulo
+    select @codigo_articulo = codigo_articulo from inserted;
+
+    -- inserted es la tabla temporal del insert
+
+END
+GO
+
