a9909da5d46cdf78d5ec56d44c2217167c5b85f1
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-25 15:32:12 -0400

    Update CA 5 Trigers.sql

--- Actividades SQL/CA 5 Trigers.sql
+++ Actividades SQL/CA 5 Trigers.sql
@@ -83,10 +83,53 @@ BEGIN
     SET NOCOUNT ON      -- previene que existan resultados extras que interfieran con el trigger
 
     -- actualizar el stock de una sucursal, se necesita cod_sucursal y cod_articulo
-    select @ca = codigo_articulo from inserted; -- a la par que se recupera se guarda en ca
-    select @anio = anho from inserted;
-    select @num = numero_pedido from inserted;
-    select @cant = cantidad from inserted;
+    select @ca = codigo_articulo from deleted; -- a la par que se recupera se guarda en ca
+    select @anio = anho from deleted;
+    select @num = numero_pedido from deleted;
+    select @cant = cantidad from deleted;
+
+    -- inserted es la tabla temporal del insert  (en este caso es copia de predidos_articulos)
+
+    select @cs = codigo_sucursal from pedidos     -- codigo_sucursal se recupera de pedidos
+    where anho = @anio and numero_pedido = @num
+
+    -- obtener la cantidad que tenemos en articulo_sucursal (se recupera)
+    select @cantart = stock from articulos_sucursal
+    where codigo_articulo = @ca and codigo_sucursal = @cs
+
+    if (@cant < @cantart)     -- si cantidad a vender es menor a cantidad que tengo
+    begin
+        -- dato a actualizar del stock. (update sobre articulos_sucursal para el campo stock)
+        update articulos_sucursal
+        set stock = stock - @cant            -- una venta, osea aumenta el pedido y baja el stock
+        where codigo_articulo = @ca and codigo_sucursal = @cs
+    end
+        
+END
+GO
+
+
+-- creacion de un trigger para update
+CREATE TRIGGER upd_stock               -- nombre del trigger
+    ON pedidos_articulos               -- se indica sobre que tabla debe actuar
+    AFTER UPDATE                       -- que accion disparara este trigger
+AS 
+DECLARE
+    @ca int,
+    @anio int,
+    @num int,
+    @cs int,
+    @cant int,
+    @cantart int
+BEGIN
+
+    SET NOCOUNT ON      -- previene que existan resultados extras que interfieran con el trigger
+
+    -- actualizar el stock de una sucursal, se necesita cod_sucursal y cod_articulo
+    select @ca = codigo_articulo from deleted; -- a la par que se recupera se guarda en ca
+    select @anio = anho from deleted;
+    select @num = numero_pedido from deleted;
+    select @cant = cantidad from deleted;
 
     -- inserted es la tabla temporal del insert  (en este caso es copia de predidos_articulos)
 
