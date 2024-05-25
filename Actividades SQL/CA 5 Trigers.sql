2628d919b895be8c63fdaeb9ee9f8a6a284a7248
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-25 14:55:57 -0400

    Update CA 5 Trigers.sql

--- Actividades SQL/CA 5 Trigers.sql
+++ Actividades SQL/CA 5 Trigers.sql
@@ -1,4 +1,5 @@
--- el triger se hace sobre la tabla que queremos disparar el triger
+-- el triger se hace sobre la tabla en la que queremos disparar el triger, 
+-- la del calculo es otra tabla
 
 -- creacion de un trigger para insertar
 CREATE TRIGGER ins_stock               -- nombre del trigger
@@ -6,16 +7,35 @@ CREATE TRIGGER ins_stock               -- nombre del trigger
     AFTER INSERT                       -- que accion disparara este trigger
 AS 
 DECLARE
-    @ca int
+    @ca int,
+    @anio int,
+    @num int,
+    @cs int,
+    @cant
 BEGIN
 
     SET NOCOUNT ON      -- previene que existan resultados extras que interfieran con el trigger
 
     -- actualizar el stock de una sucursal, se necesita cod_sucursal y cod_articulo
     select @ca = codigo_articulo from inserted; -- a la par que se recupera se guarda en ca
+    select @anio = anho from inserted;
+    select @num = numero_pedido from inserted;
+    select @cant = cantidad from inserted;
 
     -- inserted es la tabla temporal del insert
 
+    select @cs = codigo_sucursal from pedidos
+    where anho = @anio and numero_pedido = @num
+
+    -- la fila a actualizar
+    select * from articulos_sucursal
+    where codigo_articulo = @ca and codigo_sucursal = @cs
+        
+    -- dato a actualizar del stock. (update sobre articulos_sucursal para el campo stock)
+    update articulos_sucursal
+    set stock = stock - @cant            -- una venta, osea aumenta el pedido y baja el stock
+
+        
 END
 GO
 
