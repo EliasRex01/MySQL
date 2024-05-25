c5224d5e0d67e7892f403a0e1eaf3ec19c7c0279
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-25 19:08:42 -0400

    Update CA 5 Trigers.sql

--- Actividades SQL/CA 5 Trigers.sql
+++ Actividades SQL/CA 5 Trigers.sql
@@ -171,7 +171,31 @@ RETURNS TRIGGGER AS $fnc_act_total_pedidos$
                 montoiva = COALESCE(montoiva, 0) + ((NEW.PRECIO_VENTA * NEW.CANTIDAD) - 
                                                     ((NEW.PRECIO_VENTA * NEW.CANTIDAD) / 
                                                     (1 + COALESCE(vPorcentaje_iva, 0)))) 
-
             WHERE anho            = NEW.anho
             AND numero_pedido     = NEW.numero_pedido;
 
+            RETURN NEW;
+        END IF;
+
+        IF (TG_OP = 'UPDATE') THEN
+            IF NEW.cantidad <> OLD.cantidad OR NEW.precio_venta <> OLD.precio_venta
+            THEN
+                SELECT porcentaje_iva
+                INTO vPorcentaje_iva
+                FROM articulos
+                WHERE codigo_articulo = NEW.codigo_articulo;
+
+                UPDATE pedidos
+                    SET total = COALESCE(total, 0) - (OLD.PRECIO_VENTA * OLD.CANTIDAD)
+                    + (NEW.PRECIO_VENTA * NEW.CANTIDAD),
+                    montoiva = COALESCE(montoiva, 0) - ((OLD.PRECIO_VENTA * OLD.CANTIDAD) 
+                    - ((OLD.PRECIO_VENTA * OLD.CANTIDAD) / (1 + COALESCE(vPorcentaje_iva, 0
+                    ))))
+                    + ((NEW.PRECIO_VENTA * NEW.CANTIDAD) - ((NEW.PRECIO_VENTA * NEW.CANTIDAD)
+                    / (1 + COALESCE(vPorcentaje_iva, 0))))
+                WHERE anho            = NEW.anho
+                AND numero_pedido     = NEW.numero_pedido;
+
+                RETURN NEW;
+            END IF;
+        END IF;
