21df43582d0b7f8569153afa41a14110d93c6581
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-26 21:07:37 -0400

    Update Trigger para Actualizar Totales.sql

--- Actividades SQL/Trigger para Actualizar Totales.sql
+++ Actividades SQL/Trigger para Actualizar Totales.sql
@@ -98,3 +98,15 @@ UPDATE pedidos_articulos SET cantidad = 2
 
 -- para modificar el trigger se puede modiicar la funcion o modificar el trigger directo
 -- 
+-- todos los clientes
+SELECT cl.ruc, cl.nombre, SUM(p.total) AS TotalPedidos
+FROM clientes cl JOIN pedidos p ON cl.ruc = p.ruc
+GROUP BY cl.ruc, cl.nombre
+ORDER BY 3 DESC;
+
+-- si quiero eso para reporte se usa una vista 
+CREATE VIEW vClientesPedidos AS
+SELECT cl.ruc, cl.nombre, SUM(p.total) AS TotalPedidos
+FROM clientes cl JOIN pedidos p ON cl.ruc = p.ruc
+GROUP BY cl.ruc, cl.nombre
+ORDER BY 3 DESC;
