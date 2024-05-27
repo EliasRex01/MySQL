a95aa87b8a920ff5247b12faba04289fd8b37982
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-26 21:12:13 -0400

    Update Trigger para Actualizar Totales.sql

--- Actividades SQL/Trigger para Actualizar Totales.sql
+++ Actividades SQL/Trigger para Actualizar Totales.sql
@@ -111,5 +111,5 @@ FROM clientes cl JOIN pedidos p ON cl.ruc = p.ruc
 GROUP BY cl.ruc, cl.nombre
 ORDER BY 3 DESC;
 
--- uso de la vista
+-- uso de la vista (se puede hacer join tambien)
 SELECT * FROM vClientesPedidos WHERE nombre LIKE "Lopez";
