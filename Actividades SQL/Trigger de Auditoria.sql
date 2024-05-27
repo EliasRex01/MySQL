7d9cdba7d9efabea2800d029908531e8eb568cb7
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-27 13:00:29 -0400

    Update Trigger de Auditoria.sql

--- Actividades SQL/Trigger de Auditoria.sql
+++ Actividades SQL/Trigger de Auditoria.sql
@@ -2,12 +2,14 @@
 -- en la auditoria indicar cual es la fila que se modifico, con el dato anterior 
 -- y cual es la fila con el dato nuevo
 
+-- version del sql server
 ALTER TRIGGER articulos_UPDATE ON articulos AFTER UPDATE
 AS 
 INSERT INTO (codigo_articulo, descripcion, ultimo_costo, costo_promedio, precio_unitario,
     porcentaje_iva, codigo_tipo, codigo_proveedor)
 SELECT (codigo_articulo, descripcion, ultimo_costo, costo_promedio, precio_unitario,
     porcentaje_iva, codigo_tipo, codigo_proveedor)
+    'DELETE'.GETDATE().SUSER.$NAME
 FROM deleted;
 
 ALTER TRIGGER articulos_UPDATE ON articulos AFTER UPDATE
@@ -19,9 +21,17 @@ SELECT (codigo_articulo, descripcion, ultimo_costo, costo_promedio, precio_unita
 FROM deleted;
 
 
--- select a tabla auditoria
+-- select la tabla auditoria
 select * from articulosAUDIT;
 
 
 -- en el insert se puede usar un select para insertar valores
 -- en el update se puede usar select para modificar valores
+
+update articulos set descripcion = 
+where codigo_articulo 
+
+
+
+
+
