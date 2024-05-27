2e7d82ebbd83f24d8d27a5476ee0cce1ccbe7484
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-26 21:19:44 -0400

    Update Trigger de Auditoria.sql

--- Actividades SQL/Trigger de Auditoria.sql
+++ Actividades SQL/Trigger de Auditoria.sql
@@ -1,1 +1,6 @@
--- como hacer el trigger de auditoria
+-- guardar los datos viejos y los nuevos
+-- en la auditoria indicar cual es la fila con el dato anterior y cual es la fila
+-- con el dato nuevo
+
+
+
