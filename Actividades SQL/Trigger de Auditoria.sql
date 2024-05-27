f30bbf581fea06c704fc440425c890379b9c8c32
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-27 11:06:40 -0400

    Update Trigger de Auditoria.sql

--- Actividades SQL/Trigger de Auditoria.sql
+++ Actividades SQL/Trigger de Auditoria.sql
@@ -2,5 +2,13 @@
 -- en la auditoria indicar cual es la fila con el dato anterior y cual es la fila
 -- con el dato nuevo
 
+ALTER TRIGGER articulos_UPDATE ON articulos AFTER UPDATE
+AS 
+INSERT INTO (codigo_articulo, descripcion, ultimo_costo, costo_promedio, precio_unitario,
+    porcentaje_iva, codigo_tipo, codigo_proveedor)
+SELECT (codigo_articulo, descripcion, ultimo_costo, costo_promedio, precio_unitario,
+    porcentaje_iva, codigo_tipo, codigo_proveedor)
+
+
 
 
