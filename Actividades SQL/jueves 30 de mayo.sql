a680d4c334b1a705c34c49787ffeedd90f961f0a
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-30 20:10:22 -0400

    Update jueves 30 de mayo.sql

--- Actividades SQL/jueves 30 de mayo.sql
+++ Actividades SQL/jueves 30 de mayo.sql
@@ -1,5 +1,11 @@
 # cual es el numero de cheques
 select * from chequera where numero_chequera = 1;
 
+-- seleccionar el ultimo insertado en numero_chequera
+select max() from chequera
+where numero_chequera =1 and fecha;
 
-
+select * from chequera
+where numero_chequera and fecha
+order by 1 desc
+limit 1;
